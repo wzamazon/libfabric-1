b0f0ad45120107dcee3ecb8983e3513b5f7ff01f
Wei Zhang <wzam@amazon.com>
2019-10-16 20:49:50 +0000

    prov/efa: implement atomic, fetch_atomic and compare_atomic
    This patch add implements of fi_atomic to EFA provider.
    Beside the functions required by libfabric standard, the
    following addtional changes have been made:
    
    1. Added two data structures:
       a. rxr_atomic_hdr, which contain atomic_op and datatype,
          and will be transfered over wire
       b. rxr_atomic_ex, which contain response iov and compare
          iov and is not included in fi_msg_atomic.
    
    2. inter node atomic request is sent by two REQ packet types,
           RXR_WRITE_RTA_PKT, RXR_READCOMP_RTA_PKT
    
    3. intra node atomic request is sent by calling atomic
       functions on shm EP directly. the function
            rxr_cq_handle_shm_rma_write_data()
       has been modified and renamed to
            rxr_cq_handle_shm_completion()
       to handle shm cq_entry from atomic.
    
    4. atomrsp packet type, which involves adding the following
       functions:
            rxr_atomic_init_atomrsp_pkt_entry()
            rxr_atomic_handle_atomrsp_sent()
            rxr_atomic_handle_atomrsp_send_completion()
            rxr_atomic_recv_atomrsp_pkt()
    
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- prov/efa/src/rxr/rxr.h
+++ prov/efa/src/rxr/rxr.h
@@ -203,6 +203,7 @@ struct rxr_env {
 	int tx_max_credits;
 	int tx_queue_size;
 	int enable_sas_ordering;
+	int enable_atomic_ordering;
 	int enable_shm_transfer;
 	int shm_av_size;
 	int shm_max_medium_size;
@@ -269,6 +270,7 @@ enum rxr_rx_comm_type {
 	RXR_RX_QUEUED_EOR,	/* rx_entry was unable to send EOR over shm */
 	RXR_RX_QUEUED_CTS_RNR,	/* rx_entry RNR sending CTS */
 	RXR_RX_WAIT_READ_FINISH, /* rx_entry wait for send to finish, FI_READ */
+	RXR_RX_WAIT_ATOMRSP_SENT, /* rx_entry wait for atomrsp packet sent completion */
 };
 
 enum rxr_peer_state {
@@ -323,6 +325,24 @@ struct rxr_queued_ctrl_info {
 	int inject;
 };
 
+struct rxr_atomic_hdr {
+	/* atomic_op is different from tx_op */
+	uint32_t atomic_op;
+	uint32_t datatype;
+};
+
+/* extra information that is not included in fi_msg_atomic
+ * used by fetch atomic and compare atomic.
+ *     resp stands for response
+ *     comp stands for compare
+ */
+struct rxr_atomic_ex {
+	struct iovec resp_iov[RXR_IOV_LIMIT];
+	int resp_iov_count;
+	struct iovec comp_iov[RXR_IOV_LIMIT];
+	int comp_iov_count;
+};
+
 struct rxr_rx_entry {
 	/* Must remain at the top */
 	enum rxr_x_entry_type type;
@@ -341,6 +361,8 @@ struct rxr_rx_entry {
 	 */
 	uint32_t rma_loc_tx_id;
 	uint32_t rma_initiator_rx_id;
+
+	struct rxr_atomic_hdr atomic_hdr;
 
 	uint32_t msg_id;
 
@@ -390,6 +412,8 @@ struct rxr_rx_entry {
 	struct rxr_rx_entry *master_entry;
 
 	struct rxr_pkt_entry *unexp_pkt;
+	struct rxr_pkt_entry *atomrsp_pkt;
+	char *atomrsp_buf;
 
 #if ENABLE_DEBUG
 	/* linked with rx_pending_list in rxr_ep */
@@ -442,6 +466,10 @@ struct rxr_tx_entry {
 	/* App-provided reg descriptor */
 	void *desc[RXR_IOV_LIMIT];
 
+	/* atomic related variables */
+	struct rxr_atomic_hdr atomic_hdr;
+	struct rxr_atomic_ex atomic_ex;
+
 	/* Only used with mr threshold switch from memcpy */
 	size_t iov_mr_start;
 	struct fid_mr *mr[RXR_IOV_LIMIT];
@@ -471,7 +499,7 @@ struct rxr_domain {
 	struct util_domain util_domain;
 	struct fid_domain *rdm_domain;
 	struct fid_domain *shm_domain;
-
+	size_t mtu_size;
 	size_t addrlen;
 	uint8_t mr_local;
 	uint64_t rdm_mode;
@@ -791,6 +819,23 @@ static inline int rxr_need_sas_ordering(struct rxr_ep *ep)
 		rxr_env.enable_sas_ordering);
 }
 
+static inline int rxr_need_atomic_ordering(struct rxr_ep *ep)
+{
+	/*
+	 * RxR needs to reorder RTS packets for send-after-send guarantees
+	 * only if the application requested it and the core provider does not
+	 * support it.
+	 */
+	uint64_t atomic_ordering;
+
+	atomic_ordering = FI_ORDER_ATOMIC_RAR | FI_ORDER_ATOMIC_RAW |
+			  FI_ORDER_ATOMIC_WAR | FI_ORDER_ATOMIC_WAW;
+
+	return ((ep->msg_order & atomic_ordering) &&
+		!(ep->core_msg_order & FI_ORDER_SAS) &&
+		rxr_env.enable_atomic_ordering);
+}
+
 /* Initialization functions */
 void rxr_reset_rx_tx_to_core(const struct fi_info *user_info,
 			     struct fi_info *core_info);
@@ -852,6 +897,10 @@ void rxr_cq_write_tx_completion(struct rxr_ep *ep,
 
 void rxr_cq_handle_tx_completion(struct rxr_ep *ep,
 				 struct rxr_tx_entry *tx_entry);
+
+void rxr_cq_handle_shm_completion(struct rxr_ep *ep,
+				  struct fi_cq_data_entry *cq_entry,
+				  fi_addr_t src_addr);
 
 int rxr_cq_reorder_msg(struct rxr_ep *ep,
 		       struct rxr_peer *peer,
