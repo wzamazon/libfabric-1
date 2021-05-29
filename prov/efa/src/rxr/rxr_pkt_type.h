dc304918b9a7f71329c2a6015a38b12b0243149d
Wei Zhang <wzam@amazon.com>
2021-05-29 14:19:38 +0000

    prov/efa: add optional qkey header to receipt packet
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- prov/efa/src/rxr/rxr_pkt_type.h
+++ prov/efa/src/rxr/rxr_pkt_type.h
@@ -426,6 +426,7 @@ struct rxr_receipt_hdr {
 	uint32_t tx_id;
 	uint32_t msg_id;
 	int32_t padding;
+	char opt_hdr[0];
 };
 
 static inline
@@ -433,6 +434,8 @@ struct rxr_receipt_hdr *rxr_get_receipt_hdr(void *pkt)
 {
 	return (struct rxr_receipt_hdr *)pkt;
 }
+
+#define RXR_RECEIPT_OPT_QKEY_HDR BIT_ULL(0)
 
 /* receipt packet functions: init, handle_sent, handle_send_completion, recv*/
 int rxr_pkt_init_receipt(struct rxr_ep *ep, struct rxr_rx_entry *rx_entry,
