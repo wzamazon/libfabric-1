c41cb7ec74aad55dd0f297b3b572cf50222c7118
Wei Zhang <wzam@amazon.com>
2021-06-12 23:57:54 +0000

    prov/efa: allocate memory for unexpected packet pool during ep initialization
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- prov/efa/src/rxr/rxr_ep.c
+++ prov/efa/src/rxr/rxr_ep.c
@@ -1089,6 +1089,14 @@ int rxr_ep_init(struct rxr_ep *ep)
 
 		if (ret)
 			goto err_free;
+
+		ret = ofi_bufpool_grow(ep->rx_unexp_pkt_pool);
+		if (ret) {
+			FI_WARN(&rxr_prov, FI_LOG_CQ,
+				"cannot allocate memory for unexpected packet pool. error: %s\n",
+				strerror(-ret));
+			goto err_free;
+		}
 	}
 
 	if (rxr_env.rx_copy_ooo) {
